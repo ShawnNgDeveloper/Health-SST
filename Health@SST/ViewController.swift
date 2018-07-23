0824c42c019fa5279ab2400e630567dd14a7fd12
Shawn Ng Y K <shawnng@shawnthesheepsmacbookpro2017-2.local>
2018-07-23 15:19:42 +0800

    Initial Commit

---
+++ Health@SST/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Health@SST
+//
+//  Created by Shawn Ng Y K on 23/7/18.
+//  Copyright © 2018 SST. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
